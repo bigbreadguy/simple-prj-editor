bd40afdcc5a364c0f6b1b688ae5e8cb8aa303619
bigbreadguy <50568142+bigbreadguy@users.noreply.github.com>
2024-08-28 19:32:15 +0900

    FEAT: implement file upload and data editor interface

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
+from src.functions import parse_prj
+
 st.title("Simple ContamW project file editor")
+
+prj_file = st.file_uploader("Choose a .prj file", type=["prj"])
+if prj_file is not None:
+    df, col_name_string, target_data, pre_target_data, post_target_data = parse_prj(prj_file)
+
+    edited_df = st.dataframe(df)
