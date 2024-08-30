0c8c6f7141875538b442c0dfaa1906c9eb901605
bigbreadguy <50568142+bigbreadguy@users.noreply.github.com>
2024-08-30 18:09:34 +0900

    FEAT: implement mito as dataframe editor

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from mitosheet.streamlit.v1 import spreadsheet
 import streamlit as st
 
 from src.functions import parse_prj
@@ -8,9 +9,11 @@ prj_file = st.file_uploader("Choose a .prj file", type=["prj"])
 if prj_file is not None:
     df, col_name_string, target_data, pre_target_data, post_target_data = parse_prj(prj_file)
 
-    edited_df = st.data_editor(df)
-    target_string = edited_df.to_csv(header=False, index=False, sep="\t")
+    edited_df, code = spreadsheet(df)
+    # st.subheader("Display the code that corresponds to the script")
+    # st.code(code)
 
+    target_string = edited_df["df1"].to_csv(header=False, index=False, sep="\t")
     full_string = pre_target_data + col_name_string + target_string + post_target_data
     full_data = full_string.encode("CP949")
 
@@ -20,3 +23,6 @@ if prj_file is not None:
         file_name=prj_file.name,
         mime="text/plain",
     )
+
+    # st.subheader("Edited data in ordered dict form", divider=True)
+    # st.write(edited_df)
