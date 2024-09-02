7667bacb5fa048d3854ba6de2bc742a8c3114ba2
bigbreadguy <50568142+bigbreadguy@users.noreply.github.com>
2024-09-02 16:36:08 +0900

    CHORE: use join data function in the application

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 from mitosheet.streamlit.v1 import spreadsheet
 import streamlit as st
 
-from src.functions import parse_prj
+from src.functions import parse_prj, join_data
 
 st.title("Simple ContamW project file editor")
 
@@ -13,8 +13,11 @@ if not os.path.isdir(data_dir):
 
 prj_file = st.file_uploader("Choose a .prj file", type=["prj"])
 if prj_file is not None:
+    # Parse data from the uploaded .prj file
     (
         flow_paths,
+        zones,
+        zones_col_name_string,
         flow_paths_col_name_string,
         header_string,
         flow_elements_string,
@@ -23,25 +26,26 @@ if prj_file is not None:
         footer_string
     ) = parse_prj(prj_file)
 
+    # Merge and display dataframes
+
     edited_dfs, code = spreadsheet(
         flow_paths,
         import_folder=data_dir
     )
+    flow_paths = edited_dfs.popitem(last=False)[1]
 
-    flow_paths_string = edited_dfs.popitem(
-        last=False
-    )[1].to_csv(header=False, index=False, sep="\t")
-    full_string = (
-            header_string
-            + flow_elements_string
-            + other_elements_string
-            # + zones_string
-            + initial_zone_concentrations_string
-            + flow_paths_col_name_string
-            + flow_paths_string
-            + footer_string
+    # Join data into a string
+    full_data = join_data(
+        flow_paths,
+        zones,
+        zones_col_name_string,
+        flow_paths_col_name_string,
+        header_string,
+        flow_elements_string,
+        other_elements_string,
+        initial_zone_concentrations_string,
+        footer_string
     )
-    full_data = full_string.encode("CP949")
 
     st.download_button(
         label="Download edited data as prj file",
