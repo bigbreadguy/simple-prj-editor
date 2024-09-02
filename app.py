642d83b9d3d222e1227dc0f34f23e601fd01e28b
bigbreadguy <50568142+bigbreadguy@users.noreply.github.com>
2024-09-02 17:04:18 +0900

    FEAT: merge zones into flow paths

--- app.py
+++ app.py
@@ -27,16 +27,34 @@ if prj_file is not None:
     ) = parse_prj(prj_file)
 
     # Merge and display dataframes
+    zones_target = zones[["Z#", "name"]]
+
+    merged_data = flow_paths.merge(
+        zones_target, how="left", left_on="n#", right_on="Z#")
+    merged_data.rename(columns={"name": "존 이름(from)"}, inplace=True)
+    merged_data.drop(columns=["Z#"], inplace=True)
+
+    merged_data = merged_data.merge(
+        zones_target, how="left", left_on="m#", right_on="Z#")
+    merged_data.rename(columns={"name": "존 이름(to)"}, inplace=True)
+    merged_data.drop(columns=["Z#"], inplace=True)
+
+    merged_data["존 이름(from)"] = merged_data.apply(
+        lambda row: "외기" if row["n#"] == -1 else row["존 이름(from)"], axis=1)
+    merged_data["존 이름(to)"] = merged_data.apply(
+        lambda row: "외기" if row["m#"] == -1 else row["존 이름(to)"], axis=1)
 
     edited_dfs, code = spreadsheet(
-        flow_paths,
+        merged_data,
         import_folder=data_dir
     )
-    flow_paths = edited_dfs.popitem(last=False)[1]
+    merged_data = edited_dfs.popitem(last=False)[1]
+    flow_paths_edited = merged_data.drop(
+        columns=["존 이름(from)", "존 이름(to)"], inplace=False)
 
     # Join data into a string
     full_data = join_data(
-        flow_paths,
+        flow_paths_edited,
         zones,
         zones_col_name_string,
         flow_paths_col_name_string,
